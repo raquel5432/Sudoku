be6cd01a46715fbf2d06987beafb49d9615779f8
dhsib-XD <carlosgeorodriguez11@gmail.com>
2025-10-31 08:47:35 -0600

    Clase abstract

---
+++ a/src/Sbase.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author CarlosXl
+ */
+public abstract class Sbase {
+    
+    int sz = 9;
+    protected int tblero [][] = new int[sz][sz];
+    
+    
+    
+}
